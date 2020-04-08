97833af6046fda48bb0a7ca8493c0d7da09cb696
Akarsh Goel <akarshgoel1997@gmail.com>
2020-04-09 03:07:23 +0530

    Updates

---
+++ precache-manifest.a26306dd2fb881a77c2b7d7c4d712daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d19cf7352ecb09b46793dca0afcfba2",
+    "url": "/version-release/index.html"
+  },
+  {
+    "revision": "9ccfad3df6c4b689ff07",
+    "url": "/version-release/static/css/main.4b409775.chunk.css"
+  },
+  {
+    "revision": "cab8277dae5a637b8ae9",
+    "url": "/version-release/static/js/2.d1c6b260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/version-release/static/js/2.d1c6b260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ccfad3df6c4b689ff07",
+    "url": "/version-release/static/js/main.7c29d1ac.chunk.js"
+  },
+  {
+    "revision": "fafaef1fa885641fa8a0",
+    "url": "/version-release/static/js/runtime-main.10c619a9.js"
+  }
+]);
