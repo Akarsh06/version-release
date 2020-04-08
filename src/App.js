52327e603be6b47d14868cded317ebb07ed22b81
Akarsh Goel <akarshgoel1997@gmail.com>
2020-04-09 03:08:07 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ExampleChart from './chart/chart.component';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='g-chart'>
+      <h2>dKOSH Version Release Chart</h2>
+      <ExampleChart />
+      </div>
     </div>
   );
 }
---
+++ src/chart/chart.component.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { Chart } from "react-google-charts";
+import './chart.style.css';
+
+const ExampleChart = () => {
+  return (
+    <Chart
+  width={'100%'}
+  height={'500px'}
+  chartType="Timeline"
+  loader={<div>Loading Chart</div>}
+  data={[
+    [
+      { type: 'string', id: 'Name' },
+      { type: 'string', id: 'Phase' },
+      { type: 'date', id: 'Start' },
+      { type: 'date', id: 'End' },
+    ],
+    [
+      'dKOSH V5.0',
+      'Design',
+      new Date(2020, 4, 1),
+      new Date(2020, 8, 1),
+    ],
+    [
+      'dKOSH V5.0',
+      'Development',
+      new Date(2020, 8, 1),
+      new Date(2021, 0, 1),
+    ],
+    [
+      'dKOSH V5.0',
+      'Testing',
+      new Date(2021, 0, 1),
+      new Date(2021, 4, 1),
+    ],
+    [
+      'dKOSH V4.0',
+      'Design',
+      new Date(2020, 0, 1),
+      new Date(2020, 4, 1),
+    ],
+    [
+      'dKOSH V4.0',
+      'Development',
+      new Date(2020, 4, 1),
+      new Date(2020, 8, 1),
+    ],
+    [
+      'dKOSH V4.0',
+      'Testing',
+      new Date(2020, 8, 1),
+      new Date(2021, 0, 1),
+    ],
+    [
+      'dKOSH V3.0',
+      'Design',
+      new Date(2019, 8, 1),
+      new Date(2020, 0, 1),
+    ],
+    [
+      'dKOSH V3.0',
+      'Development',
+      new Date(2020, 0, 1),
+      new Date(2020, 4, 1),
+    ],
+    [
+      'dKOSH V3.0',
+      'Testing',
+      new Date(2020, 4, 1),
+      new Date(2020, 8, 1),
+    ],
+    [
+      'dKOSH V2.0',
+      'Design',
+      new Date(2019, 4, 1),
+      new Date(2019, 8, 1),
+    ],
+    [
+      'dKOSH V2.0',
+      'Development',
+      new Date(2019, 8, 1),
+      new Date(2020, 0, 1),
+    ],
+    [
+      'dKOSH V2.0',
+      'Testing',
+      new Date(2020, 0, 1),
+      new Date(2020, 3, 31),
+    ],
+    [
+      'dKOSH V1.0',
+      'Design',
+      new Date(2019, 0, 1),
+      new Date(2019, 4, 1),
+    ],
+    [
+      'dKOSH V1.0',
+      'Development',
+      new Date(2019, 4, 1),
+      new Date(2019, 8, 1),
+    ],
+    [
+      'dKOSH V1.0',
+      'Testing',
+      new Date(2019, 8, 1),
+      new Date(2020, 0, 1),
+    ],
+  ]}
+  options={{
+    colors: ['#e95420', '#603913', '#c69c6e'],
+    timeline: {
+      rowLabelStyle: {
+        fontName: 'Roboto',
+        fontSize: 14,
+        color: '#603913',
+      },
+      barLabelStyle: { fontName: 'Roboto', fontSize: 16 },
+    },
+    backgroundColor: '#ffffff',
+  }}
+  rootProps={{ 'data-testid': '7' }}
+/>
+  );
+};
+
+export default ExampleChart;
